d83705f4120d114d4a60c819f373f27ff36699a8
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-05-29 17:35:57 +0200

    struttura del progetto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 var km = prompt ('Inserire il numero di chilometri che vuole percorrere');
+// var day = promt (' Inserire il giorno di nascita del viaggiatore');
+// var month = prompt ('Inserire il mese di nascita del viaggiatore')
+// var year = prompt ('Inserire l\'anno di nascita del viaggiatore')
 var age = prompt('Inserire l\'età del passaggero');
 
 var priceElement = document.getElementById('price');
@@ -9,10 +12,10 @@ var salePensioner = price - (price / 100 * 40);
 
 
 if (age < 18) {
-    priceElement.innerHTML = saleMinor ;
-} else if (age >= 65)   { 
-    priceElement.innerHTML = salePensioner ;
+    priceElement.innerHTML = 'L\'importo totale da pagare:'  + saleMinor + ('€');
+} else if (age > 65)   { 
+    priceElement.innerHTML = 'L\'importo totale da pagare:'  + salePensioner + ('€');
 
 } else {
-   priceElement.innerHTML = price;
+   priceElement.innerHTML = 'L\'importo totale da pagare:'  + price + ('€');
 }
