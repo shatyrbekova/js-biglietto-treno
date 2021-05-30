6ff9f24befe6ae4c353d01dcb69f5c8253206ec6
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-05-30 18:57:56 +0200

    struttura

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 var km = prompt ('Inserire il numero di chilometri che vuole percorrere');
-// var day = promt (' Inserire il giorno di nascita del viaggiatore');
-// var month = prompt ('Inserire il mese di nascita del viaggiatore')
-// var year = prompt ('Inserire l\'anno di nascita del viaggiatore')
-var age = prompt('Inserire l\'età del passaggero');
+var year = prompt('Inserire l\'anno di nascita');
 
+var today = new Date();
+var age =  today.getFullYear() - year;
+
+var salePensionaer = sp;
+var saleMinor = sm;
 var priceElement = document.getElementById('price');
-
 var price = km * 0.21;
-var saleMinor = price - (price / 100 * 20);
-var salePensioner = price - (price / 100 * 40);
+var sm = price - (price / 100 * 20);
+var sp = price - (price / 100 * 40);
+
 
 
 if (age < 18) {
-    priceElement.innerHTML = 'L\'importo totale da pagare:'  + saleMinor + ('€');
+    priceElement.innerHTML = 'L\'importo totale da pagare:'  + ' ' + sm + ('€');
 } else if (age > 65)   { 
-    priceElement.innerHTML = 'L\'importo totale da pagare:'  + salePensioner + ('€');
+    priceElement.innerHTML = 'L\'importo totale da pagare:'  + ' ' + sp + ('€');
 
 } else {
-   priceElement.innerHTML = 'L\'importo totale da pagare:'  + price + ('€');
+   priceElement.innerHTML = 'L\'importo totale da pagare:'  + ' ' + price + ('€');
 }
